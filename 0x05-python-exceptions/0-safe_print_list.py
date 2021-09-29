0e4040a47721f598f0aa152f2a134c65915460ad
yusuf <horiyorrmi72@gmail.com>
2021-09-29 09:42:39 -0700

    list_integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-    while idx < x:
-        try:
-            print("{}".format(my_list[idx]), end="")
-        except IndexError:
-            break
-        idx += 1
-    print("")
-    return idx
+        cmpt = 0
+            for index in range(x):
+                        try:
+                                        print(my_list[index], end='')
+                                                    cmpt += 1
+                        except Exception as error:
+                                        break
+                                        print('')
+                                            return cmpt
