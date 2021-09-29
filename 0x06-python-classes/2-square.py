cb0a2f334e112a3301055f8bb3fc4ac20bfb3014
yusuf <horiyorrmi72@gmail.com>
2021-09-29 10:59:04 -0700

    upgd

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-This module defines a squre class
-
-It implements value and type cheks for its attributes
+This module defines a Square class
+Its implements value and type checks for its attributes
 """
 
 
 class Square:
     """Square implementation
-"""
+    """
     def __init__(self, size=0):
-        it type(size) != int:
-        raise TypeError("size must be an integer")
-elif size < 0:
-    raise ValueError("size must be >= 0")
-self.__size = size
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
