44c2184523a06a180e49e963134e022ecbd7c407
yusuf <horiyorrmi72@gmail.com>
2021-12-22 05:45:54 -0800

    fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 const passedArgms = process.argv;
-let output;
-output = passedArgms[2] + ' is ' + passedArgms[3];
-console.log(output);
+console.log(passedArgms[2] + ' is ' + passedArgms[3]);
