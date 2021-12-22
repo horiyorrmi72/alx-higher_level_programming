e010f8e3bd4eb92f5c5b8ecb2684df3ce8c45030
yusuf <horiyorrmi72@gmail.com>
2021-12-22 06:10:17 -0800

    multilang loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const valuesToPrint = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < valuesToPrint.length; i++) {
+  console.log(valuesToPrint[i]);
+}
