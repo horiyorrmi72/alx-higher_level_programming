e1ecddda57ed98ea42c5d7cb700b1ab43454c27b
yusuf <horiyorrmi72@gmail.com>
2021-12-22 07:43:42 -0800

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function factorial (n) {
-  if (isNaN(n) || n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+const valuef = parseInt(processs.argv[2]);
+function factorial(n){
+	if(isNaN(n) || n === 0){
+	    return 1;
+	}else{
+	    return n * factorial(n -1);
+	}
+    console.log(factorial(n));
 }
-
-console.log(factorial(parseInt(process.argv[2])));
