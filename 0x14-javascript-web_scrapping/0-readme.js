0cb54487174e03b4212c8df886e7b67da3921154
yusuf ridwan <horiyorrmi72@gmail.com>
2022-02-01 19:47:46 -0800

    new f

--- 0x14-javascript-web_scrapping/0-readme.js
+++ 0x14-javascript-web_scrapping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err,data) => {
-    if(err) {
-	console.log(err);
-    }
-  console.log(data);
+let filename = process.argv[2];
+const fs = require('fs');
+fs.readFile(filename, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
