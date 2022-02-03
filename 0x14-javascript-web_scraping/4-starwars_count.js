3ec7bcfc6abaa91faa5e7ba8ef3d265d4eed9cd0
yusuf ridwan <horiyorrmi72@gmail.com>
2022-02-03 03:58:33 +0000

    fix semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
+const url = process.argv[2];
 const request = require('request');
 
-request(url,function(err, response, body){
-if(err){
-console.log(err);
-}else if(response.statusCode === 200){
-let films = JSON.parse(body).results;
-let count = 0;
-for(let i in films){
-let chars = films[1].characters;
-for(let c in chars){
-if(chars[c].includes('18')){
-count++;
-}
-}
-}
-console.log(count);
-}else{
-console.log('Error code:' + response.statusCode);
-}
-})
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const i in films) {
+      const chars = films[i].characters;
+      for (const c in chars) {
+        if (chars[c].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('Error code:' + response.statusCode);
+  }
+});
