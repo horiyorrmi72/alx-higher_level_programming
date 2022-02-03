155aa0fc0f5fafc009090db84050a83cf66f0310
yusuf ridwan <horiyorrmi72@gmail.com>
2022-02-03 03:30:10 +0000

    updt:

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const filename = process.argv[2];
 const text = process.argv[3];
 const fs = require('fs');
-fs.witeFile(filename, text, 'utf8', function (err) {
+fs.writeFile(filename, text, 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
